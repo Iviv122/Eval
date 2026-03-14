3fb99e9593b334cc240465c933364c80a4a6c24a
Iviv122 <ivan.skrinnik.nomia@gmail.com>
2026-03-14 20:32:52 +0100

    no collisions

--- src/evaler/logger.ts
+++ src/evaler/logger.ts
@@ -8,8 +8,8 @@ export class ASTLogger {
         this.ctx.clearRect(0, 0, 1000, 1000)
         ctx.fillStyle = "white"
 
-        console.log("depth:" + this.get_tree_depth(root));
-        this.log(root, 0, 50)
+        let depth = this.get_tree_depth(root);
+        this.log(root, 0, 50,depth)
     }
 
     get_tree_depth(node:Node): number{
@@ -59,16 +59,16 @@ export class ASTLogger {
         this.ctx.lineWidth = 1;
     }
 
-    log(node: Node, x: number, y: number) {
+    log(node: Node, x: number, y: number,depth : number) {
 
         let s = this.node_to_text(node);
         if (node.left){
-            this.place_line(x,y,x-50,y+45);
-            this.log(node.left,x-50,y+45);
+            this.place_line(x,y,x-50*depth,y+45);
+            this.log(node.left,x-50*depth,y+45,depth-1);
         } 
         if (node .right){
-            this.place_line(x,y,x+50,y+95);
-            this.log(node.right,x+50,y+95);
+            this.place_line(x,y,x+50*depth,y+45);
+            this.log(node.right,x+50*depth,y+45,depth-1);
         }
         this.place(s,x,y)
 
